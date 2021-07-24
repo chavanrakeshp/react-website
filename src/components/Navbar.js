aa1f68b5d47a385f9df4cd57ae15ed84971e8c79
creativeskills <creativeskillsdev@gmail.com>
2021-07-24 16:43:44 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <>
+           <nav className="navbar">
+               <div className="navbar-container">
+                <link to="/" className="navbar-logo"></link>
+               </div>
+           </nav>
+        </> 
+    )
+}
+
+export default Navbar
